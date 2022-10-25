ae3146c27c91fa9667be6fb1aa5c5e8d851db353
Romack Natividad <romacknatividad@gmail.com>
2022-10-26 04:32:37 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const loadMiembros = function() {
         }
         document.getElementById("miembros").innerHTML = text;
     }
-    xmlhttp.open("GET", "/attendanceform/api/app-data.json?x=10-01-2022-0224");
+    xmlhttp.open("GET", "/attendanceform/api/app-data.json?x=10-26-2022-0432");
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send("x=" + dbParam);
 }
